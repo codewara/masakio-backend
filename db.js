d40b59e2e44076092b29c2d23c1d4c34449e82c1
Daneswara <150428419+codewara@users.noreply.github.com>
2025-06-16 19:13:33 +0700

    fix connection

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql2');
 require('dotenv').config({path: '.env'});
 
-const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+host = process.env.DB_HOST;
+user = process.env.DB_USER;
+pass = process.env.DB_PASSWORD;
+database = process.env.DB_NAME;
+port = process.env.PORT;
+
+const db = mysql.createPool(`mysql://${user}:${pass}@${host}:${port}/${database}`);
 
 module.exports = db;
