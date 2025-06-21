a89e5a91234e3edd441d518f9cde48dfaad3c3bb
Daneswara <150428419+codewara@users.noreply.github.com>
2025-06-22 05:37:26 +0700

    add auth route and main endpoint to check server

--- index.js
+++ index.js
@@ -11,57 +11,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Welcome to the MASAKIO API',
-    cloudinary: cloudinary.config().cloud_name ? 'Configured' : 'Not Configured',
-  });
-});
+app.get('/', (req, res) => res.send('Welcome to the Recipe API'));
 
-// GET
-app.get('/wishlist', (req, res) => {
-  db.query(`
-    SELECT rec.id_resep, rec.nama_resep, rec.thumbnail, COUNT(rev.id_resep) as review_count, ROUND(AVG(rev.rating), 1) as rating
-    FROM wishlist w
-    JOIN resep rec ON w.id_resep = rec.id_resep
-    JOIN user u ON w.id_user = u.id_user
-    JOIN review rev ON rev.id_resep = w.id_resep
-    WHERE w.id_user = ?
-    GROUP BY rec.id_resep
-  `, [1], // Profile ID 1
-  (err, results) => {
-    if (err) return res.status(500).json({ error: err.message });
-    res.json(results);
-  });
-});
-
-app.get('/recipes', (req, res) => {
-  db.query(`
-    SELECT rec.id_resep, rec.nama_resep, rec.thumbnail, COUNT(rev.id_resep) as review_count, ROUND(AVG(rev.rating), 1) as rating
-    FROM resep rec
-    LEFT JOIN review rev ON rev.id_resep = rec.id_resep
-    GROUP BY rec.id_resep
-  `,
-  (err, results) => {
-    if (err) return res.status(500).json({ error: err });
-    res.json(results);
-  });
-});
-
-// POST
-app.post('/wish/:id', (req, res) => {
-  db.query(
-    'INSERT INTO wishlist (id_user, id_resep) VALUES (?, ?)',
-    [1, req.params.id],
-  );
-});
-
-app.post('/unwish/:id', (req, res) => {
-  db.query(
-    'DELETE FROM wishlist WHERE id_user = ? AND id_resep = ?',
-    [1, req.params.id],
-  );
-});
+app.use('/auth', require('./routes/auth'));
 
 app.listen(PORT, () => {
   db.getConnection((err) => {
